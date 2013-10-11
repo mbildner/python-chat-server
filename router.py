6796d3413f51c2af47aa3c6443df82ec4f80e2be
Moshe Bildner <moshe.bildner@gmail.com>
2013-10-11 19:34:01 -0400

    Adds redis integration.

--- router.py
+++ router.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, before_requests
 from flask_sockets import Sockets
 
 import redis
@@ -12,6 +12,10 @@ sockets = Sockets(app)
 app.debug = True
 app.reload = True
 
+@before_requests
+def before():
+	g.r = redis.StrictRedis(host="localhost", port=6379, db=0)
+
 
 @app.route('/')
 def home():
@@ -19,15 +23,15 @@ def home():
 	return resp
 
 
-
 @sockets.route('/chatsocket')
 def chatsocket(ws):
 	while True:
 		message = ws.receive()
+		g.r.rpush('messages', str(message))
 
-		reply = "received: %r" %(message)
-		ws.send(reply)
-
+		messages = g.r.lrange('messages', 0, -1)
+		for message in messages:
+			ws.send(message)
 
 if __name__ == "__main__":
 	app.run()
