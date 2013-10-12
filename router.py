01485521b4c4c65cc2c17ef2b24b701e70926455
Moshe Bildner <moshe.bildner@gmail.com>
2013-10-12 00:57:22 +0000

    WINNING WITH GEVENT

--- router.py
+++ router.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, g
 from flask_sockets import Sockets
+import gevent
 
 import redis
 
@@ -12,6 +13,18 @@ sockets = Sockets(app)
 app.debug = True
 app.reload = True
 
+def redis_to_ws(r, ws):
+	client = r.pubsub()
+	client.subscribe("msg_channel")
+	for msg in client.listen():
+		if msg['type'] == 'subscribe':
+			continue
+		ws.send(msg['data'])
+
+def ws_to_redis(r, ws):
+	message = ws.receive()
+	r.publish("msg_channel", message)
+	gevent.spawn(ws_to_redis, r, ws)
 
 @app.route('/')
 def home():
@@ -22,13 +35,10 @@ def home():
 @sockets.route('/chatsocket')
 def chatsocket(ws):
 	r = redis.StrictRedis(host="localhost", port=6379, db=0)
-	while True:
-		message = ws.receive()
-		r.rpush('messages', str(message))
-
-		messages = r.lrange('messages', 0, -1)
-		for message in messages:
-			ws.send(message)
+	gevent.joinall([
+		gevent.spawn(ws_to_redis, r, ws),
+		gevent.spawn(redis_to_ws, r, ws)
+	])
 
 if __name__ == "__main__":
 	app.run()
