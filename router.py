14c1a9348095ee304372522adcfebcb71f611637
Moshe Bildner <moshe.bildner@gmail.com>
2013-11-06 19:22:29 -0500

    Working multiplayer - single food.

--- router.py
+++ router.py
@@ -28,6 +28,12 @@ def ws_to_redis(r, ws):
 	r.publish("msg_channel", message)
 	gevent.spawn(ws_to_redis, r, ws)
 
+
+
+@app.route('/test')
+def test():
+	return "working"
+
 @app.route('/')
 def home():
 	resp = render_template('index.html')
@@ -38,6 +44,38 @@ def home():
 def demo():
 	resp = render_template('snake.html')
 	return resp
+
+
+
+def incoming_snake_update(r, ws):
+	client = r.pubsub()
+	client.subscribe("snake_update")
+	# client.listen is a blocking call
+	for msg in client.listen():
+		# new subscriptions get announced, ignore them for now
+		if msg['type'] == 'subscribe':
+			continue
+		# push the data back over our websocket connection to client
+		ws.send(msg['data'])
+
+
+def outgoing_snake_update(r, ws):
+	# ws.receive is a blocking call that will return as soon as it gets something
+	message = ws.receive()
+	if message is None:
+		return
+	r.publish("snake_update", message)
+	gevent.spawn(outgoing_snake_update, r, ws)
+
+
+@sockets.route('/snakesocket')
+def snakesocket(ws):
+	r = redis.StrictRedis(host="localhost", port=6379, db=0)
+
+	gevent.joinall([
+		gevent.spawn(incoming_snake_update, r, ws),
+		gevent.spawn(outgoing_snake_update, r, ws)
+		])
 
 
 @sockets.route('/chatsocket')
--- static/simpleSnake.js
+++ static/simpleSnake.js
@@ -23,11 +23,15 @@ var GridModel = function(canvas, rows, cols){
 	var rowHeight = canvas.height/rows;
 	var colWidth = canvas.width/cols;
 
+	this.rowHeight = rowHeight;
+	this.colWidth = colWidth;
 
 	this.colWidth = colWidth;
 	this.rowHeight = rowHeight;
 
-	this.grid = [];
+	var grid = [];
+
+	this.grid = grid;
 
 	for (var row=0; row<rows; row++){
 		var currentRow = [];
@@ -49,36 +53,44 @@ var GridModel = function(canvas, rows, cols){
 			var randCol = Math.floor(Math.random()*this.grid[0].length);
 			var randBox = this.grid[randRow][randCol];
 		}
-
-		console.log(randBox);
+		
 		return randBox;
 	}
 
 	// Canvas setup code - put it in a this.init function
 
 	// set edges to collideable and render them black
-	this.grid[0].forEach(function(box){
-		box.collideable = true;
-		box.render("black");
-	});
+	// CHANGED CODE WATCH FOR ERROR
+	this.setHardEdges = function () {
+		this.grid[0].forEach(function(box){
+			box.collideable = true;
+			box.render("black");
+		});
+		
+		this.grid[this.grid.length-1].forEach(function(box){
+			box.collideable = true;
+			box.render("black");
+		});
 
-	this.grid[this.grid.length-1].forEach(function(box){
-		box.collideable = true;
-		box.render("black");
-	});
+		this.grid.forEach(function(row){
+			row[0].collideable = true;
+			row[row.length-1].collideable = true;
+			row[0].render("black");
+			row[row.length-1].render("black");
+		});		
+	}
 
-	this.grid.forEach(function(row){
-		row[0].collideable = true;
-		row[row.length-1].collideable = true;
-		row[0].render("black");
-		row[row.length-1].render("black");
-	});
+
 
 	// set a block to have food, make it collideable, and render it blue
+
 	var foodBlock = this.randomBox();
 	foodBlock.collideable = true;
 	foodBlock.food = true;
 	foodBlock.render("blue");
+
+	this.foodBlock = foodBlock;
+	
 }
 
 
@@ -97,6 +109,9 @@ var SnakeModel = function(canvas, snakeLength){
 		"Right": {"col": 0, "row":1}
 	}	
 	
+
+
+
 	this.body = [
 		{row:1,col:3},
 		{row:1,col:4},
@@ -119,6 +134,7 @@ var SnakeModel = function(canvas, snakeLength){
 		head = this.body[0];
 		var headGridBox = gridModel.grid[head.row][head.col];
 		var collision = headGridBox.collideable;
+
 		if (collision){
 			if (headGridBox.food){
 				// grow the snake; it ate food
@@ -130,9 +146,18 @@ var SnakeModel = function(canvas, snakeLength){
 				newFood.collideable = true;
 				newFood.food = true;
 				newFood.render("blue");
+				gridModel.foodBlock = newFood;
 			} else {
+				// broadcast death to other players
+				// should build a wrapped function to only ws.send when ws.readyState === 1
+				var obit = JSON.stringify({
+					id: browserId,
+					snake: "dead",
+				});
+
 				// kill the snake, it hit something it shouldn't
 				window.clearInterval(gameLoopHandle);
+
 				alert("Game over, thanks for playing.");
 
 			}
@@ -151,17 +176,17 @@ var SnakeModel = function(canvas, snakeLength){
 
 		this.body.push(newTail);
 		var newTailBlock = gridModel.grid[newTail.row][newTail.col];
-		newTailBlock.render("lime");
+		// newTailBlock.render("lime");
 
 	}
 
 	this.direction = "Right";
 
 
-	this.body.forEach(function(block){
-		var gridBox = gridModel.grid[block.row][block.col];
-		gridBox.render("lime");
-	})
+	// this.body.forEach(function(block){
+	// 	var gridBox = gridModel.grid[block.row][block.col];
+	// 	gridBox.render("lime");
+	// })
 
 	this.move = function(direction){
 		var tail = this.body.pop();
@@ -198,19 +223,72 @@ var SnakeModel = function(canvas, snakeLength){
 		newHead.col += this.directionsDict[direction]["col"];
 		this.body = [newHead].concat(this.body);
 
-		gridModel.grid[newHead.row][newHead.col].render("lime");
+		// gridModel.grid[newHead.row][newHead.col].render("lime");
 
+	}
+
+	this.render = function (color) {
+		this.body.forEach(function (square) {
+			var box = gridModel.grid[square.row][square.col];
+			box.render(color);
+		});
 	}
 }
 
-;(function(){	
+;(function(){
+	// get a unique browser ID
+	this.browserId = Math.round(Math.random() * 1000000)
+
+	// wrap up ws init in a function
+
+	var wsEndpoint = "/snakesocket";
+
+	var origin = document.location.origin;
+
+	var wsUrl = "ws" + origin.substring(4) + wsEndpoint;
+
+	var ws = new WebSocket(wsUrl);
+
+	ws.onmessage = function (m) {
+		var update = m.data;
+		update = JSON.parse(update);
+
+		// ignore our own browser
+		if (update.id != browserId && update.snake) {
+			// this is about someone else's snake, and that snake is alive
+			outsideSnakes[update.id] = update.snake;
+		} else if (typeof(update.snake)==="undefined") {
+			console.log("snake death: ", update.id);
+		}
+	}
+
+	this.ws = ws;
+
+
 	this.canvas = document.getElementById('snakeGameCanvas');
 	this.canvas.backgroundColor = "white";
 	this.context = canvas.getContext('2d');
 	this.gridModel = new GridModel(canvas, 40, 40);	
+
+	this.gridModel.setHardEdges();
+
 	this.snake = new SnakeModel(canvas, 10);
 	
+
+	var keyDict = {
+		37 : "Left",
+		38 : "Up",
+		39 : "Right",
+		40 : "Down"
+	}
+
 	document.addEventListener("keydown", function(keyPress){
+
+		// keyidentifier should be "Up Down Right Left", mozilla doesn't implement it, so we'll do it ourselves
+
+
+		keyPress.keyIdentifier = keyDict[keyPress.which];
+
 		if (keyPress.keyIdentifier in snake.directionsDict){
 			// if the user is trying to reverse the snake's direction ignore the input
 			if ((Math.abs(snake.directionsDict[keyPress.keyIdentifier].row)) != (Math.abs(snake.directionsDict[snake.direction].row))){
@@ -222,18 +300,82 @@ var SnakeModel = function(canvas, snakeLength){
 		}
 	});
 	
-})(this)
+})(this);
 
 
+
+window.outsideSnakes = {};
+
+
+
+var clearBoard = function () {
+	// clears the board visually AND clears all collision boxes
+	gridModel.grid.forEach(function (row) {
+		row.forEach(function (square) {
+			var box = gridModel.grid[square.row][square.col];
+			box.collideable = false;
+		})
+	});
+
+	context.fillStyle = "white";
+
+	var startingX = 0 + gridModel.colWidth;
+	var startingY = 0 + gridModel.rowHeight;
+
+	var width = canvas.width;
+
+	var height = canvas.height;
+
+	context.fillRect(startingX, startingY, width, height);
+}
 
 
 var gameLoop = function(){
-	console.log('step');
+	// clear the board
+	clearBoard();
 
+	// reinstate the food
+	gridModel.foodBlock.render("blue");
+	gridModel.foodBlock.collideable = true;
+
+	// make edges crashable
+	gridModel.setHardEdges();
+	// move the snake
 	snake.move(snake.direction);
 	snake.head = snake.body[0];
+
+	// reinstate the snake
 	snake.setBodyColideable();
 	snake.collisionCheck();
+
+	// render the snake
+	snake.render("lime");
+
+	// broadcast the snake's new position
+	if (ws.readyState === 1) {
+		var snakeBody = snake.body;
+
+		var update = JSON.stringify({
+			id: browserId,
+			snake: snakeBody
+		});
+
+		ws.send(update);
+
+	}
+
+	// process incoming snakes
+	// this should be wrapped up in its own function and called
+
+	for (var s in outsideSnakes) {
+		var fsnake = outsideSnakes[s];
+
+		fsnake.forEach(function (box) {
+			var box = gridModel.grid[box.row][box.col]
+			box.collideable = true;
+			box.render("orange");
+		});
+	}
 }
 
-var gameLoopHandle = window.setInterval(gameLoop, 40);
+var gameLoopHandle = window.setInterval(gameLoop, 80);
