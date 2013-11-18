ea6f7336390d205e215f694aa50687faafca58ef
Moshe Bildner <moshe.bildner@gmail.com>
2013-11-18 14:49:08 -0500

    Working commit; users can send messages to individual users online, routed by the server.

--- router.py
+++ router.py
@@ -1,96 +1,75 @@
-# switch to more bare-bones handler for websocket suppport
-# good demo found here: https://gist.github.com/lrvick/1185629
-# flask_sockets is good, just not there yet
-
-from flask import Flask, render_template, g, request, session
-from flask_sockets import Sockets
-import gevent
-import redis
-
-
+from geventwebsocket.handler import WebSocketHandler
+from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template, session, g
+import json
 from helper import get_random_id
 
-
+import random
 
 app = Flask(__name__)
-sockets = Sockets(app)
-
-
+app.restart = True
 app.debug = True
-#app.reload = True
 
 
-app.secret_key = "moshe's super secret key"
+app.secret_key = "moshebildner"
 
 
-r = redis.Redis(host="localhost", port=8000, db=0)
+
+# use redis channels instead of this, but still should be doable
+# with dynamic ws routing.
+
+ws_targets = dict()
+
 
 
 @app.route('/')
-@app.route('/home')
 def home():
-	userid = get_random_id(20)
-	session['userid'] = userid
+
+	userid = get_random_id(10)
+	session["userid"] = userid
+	g.userid = userid;
+	# resp = render_template('home.html')
 	resp = render_template('home.html')
 	return resp
 
+# @app.route('/login', methods=["POST"])
+# def login():
+# 	username = request.form.get("username")
 
 
 
-@sockets.route('/' + userid)
-def snakesocket(ws):
-	print "connection to " + username
-	return session['userid']
+@app.route('/websocket/<target>')
+def chatsocket(target):
+	websocket = request.environ.get("wsgi.websocket", None)
 
-	while True:
-		messages = ws.listen()
+	if websocket:
+		userid = session['userid']
+		ws_targets[userid] = websocket
 
-		for message in messages:
-			print message
-			ws.send(message)
+		for ws in ws_targets.values():
+			ws.send(json.dumps({
+				"serverorders": "newusers",
+				"newusers" : ws_targets.keys()
+				}))
 
-	# if message is None:
-	# 	break
-
-
-
-
-
-
-
-
-
-
-# def outgoing_snake_update(r, ws):
-# 	# ws.receive is a blocking call that will return as soon as it gets something
-# 	message = ws.receive()
-# 	if message is None:
-# 		return
-# 	r.publish("snake_update", message)
-# 	gevent.spawn(outgoing_snake_update, r, ws)
-
-# def incoming_snake_update(r, ws):
-# 	client = r.pubsub()
-# 	client.subscribe("snake_update")
-# 	# client.listen is a blocking call
-# 	for msg in client.listen():
-# 		# new subscriptions get announced, ignore them for now
-# 		if msg['type'] == 'subscribe':
-# 			continue
-# 		# push the data back over our websocket connection to client
-# 		ws.send(msg['data'])
+		while True:
+			message = json.loads(websocket.receive())
+			target = message.get('target', 'all')
+			if target == 'all':
+				for ws in ws_targets.values():
+					ws.send(json.dumps(message))
+			else:
+				target = ws_targets[target]
+				target.send(json.dumps(message))
 
 
-# @sockets.route('/signalsocket')
-# def snakesocket(ws):
-# 	r = redis.StrictRedis(host="localhost", port=6379, db=0)
+	return "end of websocket block reached without returning a websocket"
 
-# 	gevent.joinall([
-# 		gevent.spawn(incoming_snake_update, r, ws),
-# 		gevent.spawn(outgoing_snake_update, r, ws)
-# 		])
+
+
 
 
 if __name__ == "__main__":
-	app.run()
+	http_server = WSGIServer(('', 8000), app, handler_class=WebSocketHandler)
+	http_server.serve_forever()
 
